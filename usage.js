113089adc4a3123460d14a08906519c0ffaa17c8
Claire Lesage <clairellesage@gmail.com>
2017-08-06 18:46:30 -0400

    returns alert with ingredients

--- usage.js
+++ usage.js
@@ -8,8 +8,8 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "report_back")) {
         /* Call the specified callback, passing 
            the web-pages DOM content as argument */
-	    var ingredients = document.querySelector("[id=*'ingredients']").id
-	    ingredients.scrollIntoView();
+	    var ingredients = document.querySelector("[id*=ingredients]").id
+	    // ingredients.scrollIntoView();
 		  sendResponse(ingredients);
     }
 });
