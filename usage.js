abaad4a8115e539b699beafeca49ce0d8b5d1a66
clairellesage <clairellesage@gmail.com>
2017-12-12 15:05:47 -0500

    works on wiki

--- usage.js
+++ usage.js
@@ -1,34 +1,54 @@
-// console.log("usage.js")
-// var usage = document.getElementById('Usage')
-// usage.scrollIntoView()
+function scrollIt(element) {
+  window.scrollTo({
+    'behavior': 'smooth',
+    'left': 0,
+    'top': element.offsetTop
+  });
+}
 
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing 
-           the web-pages DOM content as argument */
-      // var ingredients = $('#ingredients')
-	    var ingredientsQS = document.querySelector("[id*=ingredients]").id
-	    // var ingredients = $("document:regex(id, .*sd.*)")
+function findIngredients(queryTypes) {
+	var repeat = true;
+	for (var type in queryTypes) {
+		for (var e in elements) {
+			while (repeat) {
+				if (elements[e].className || elements[e].id) {
+					var ingredients = /[i|I]ngredients/;
+					// var ingredients = /Overview/;
+					var exactMatchId = ingredients.exec(elements[e].id)
+					var exactMatchClass = ingredients.exec(elements[e].className)
+					var textMatch = ingredients.exec(elements[e].innerHTML)
+					// alert(`${elements[e].id} ${elements[e].className}`)
+					if (textMatch) {
+						// alert(JSON.stringify(elements[e]))
+						// scrollIt(elements[e]);
+						scrollIt(elements[e]);
+						repeat = false;
+					} else if (exactMatchId) {
+						scrollIt(elements[e])
+						repeat = false;
+					}
+				}
+			}
+		}
+	}
+}
 
-	    // var ingredients = $(document)
-	    // 		.filter(function() {
-	    //         return this.id.match(/ingredient+s/);
-	    //     })
-	    //     .scrollIntoView()
-	    // ;
+var elements = document.querySelectorAll("*")
+var tags = document.getElementsByTagName("div");
+queryTypes = [elements, tags]
 
-	    if ($('#ingredients').length > 0) {
-	    	window.location.hash = "#ingredients";
-	  	} else if (ingredientsQS != null) {
-	  		alert(ingredientsQS)
-	  		window.location.hash = ingredientsQS;
-	  	} else {
-	  		alert("error!")
-	  	}
+findIngredients(queryTypes)
 
-		  sendResponse(ingredients);
-		  return true;
-    }
-});
+
+
+// var elements = document.getElementsByTagName("*");
+// var elements = document.getElementsByClassName('*')
+// const elements = document.querySelector('*');
+
+// var tags = document.getElementsByTagName('*')
+// findIngredients(tags)
+
+
+// const elm = document.querySelector('#Overview');
+
+// findIngredients(elm);
